5fdfde90108fc0f1b6141309250249ba813cab71
詹叔 <129945349+QAGITECH@users.noreply.github.com>
2024-04-09 20:09:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '𝗚𝗔𝗧𝗘𝗪𝗔𝗬𝘛𝘖𝗔𝗚𝗜.𝘊𝘖𝘔', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '𝘖𝘶𝘳𝗠𝗶𝘀𝘀𝗶𝗼𝗻丨记录𝗔𝗚𝗜诞生', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '𝘖𝘶𝘳𝗠𝗶𝘀𝘀𝗶𝗼𝗻丨精选𝗔𝗜开源项目', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://gatewaytoagi.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, AI, Blog, 笔记, AGI', // 网站关键词 英文逗号隔开
 
