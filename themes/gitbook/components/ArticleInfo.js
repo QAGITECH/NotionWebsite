c28df002695b08413406da5d9f44227a23fb4b5f
詹叔 <129945349+QAGITECH@users.noreply.github.com>
2024-05-28 18:51:20 +0800

    Update ArticleInfo.js

--- themes/gitbook/components/ArticleInfo.js
+++ themes/gitbook/components/ArticleInfo.js
@@ -2,5 +2,5 @@ export default function ArticleInfo({ post }) {
   if (!post) {
     return null
   }
-
+<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="SthAI" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
 }
