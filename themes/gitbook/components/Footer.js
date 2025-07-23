acc4a8bec87f588a08c16b1a21a5d7a66314443f
詹叔 <129945349+QAGITECH@users.noreply.github.com>
2025-07-23 12:09:12 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -13,11 +13,11 @@ const Footer = ({ siteInfo }) => {
             <hr className='pb-1' />
 
             <div className='flex justify-center'>
-                <div><i className='mx-1 animate-pulse fa-solid fa-hand-middle-finger' /> <a href={siteConfig('LINK')} className='underline text-gray-500 dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br /></div>
+                <div><i className='mx-1 animate-pulse fa-face-smile' /> <a href={siteConfig('LINK')} className='underline text-gray-500 dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br /></div>
                 © {`${copyrightDate}`}
             </div>
 
-            <div className='text-xs font-serif'>Powered By   \Donate to:  <a href='mailto:uncle.jason@qq.com' className='underline text-gray-500 dark:text-gray-300'>   𝘅𝘉𝘳𝘢𝘪𝘯.𝖾𝗍𝗁</a></div>
+            <div className='text-xs font-serif'>Donate to:  <a href='mailto:uncle.jason@qq.com' className='underline text-gray-500 dark:text-gray-300'>   𝘅𝘉𝘳𝘢𝘪𝘯.𝖾𝗍𝗁</a></div>
 
             {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br /></>}
 
