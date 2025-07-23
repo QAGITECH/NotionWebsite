f7f996ce2fd4e44a873ddb47308d9453920b3e35
詹叔 <129945349+QAGITECH@users.noreply.github.com>
2025-07-23 22:53:29 +0800

    更新 blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '𝐴𝑏𝑜𝑢𝑡𝑀𝑒', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '𝑏𝑦 𝙐𝙅', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '𝑷𝒓𝒐𝒎𝒑𝒕𝒔. 𝑭𝒍𝒐𝒘𝒔. 𝑹𝒆𝒑𝒐𝒔. 𝑷𝒂𝒑𝒆𝒓𝒔.', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://sth.ai/uj', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, AI, Blog, 笔记, AGI, 科技, 人工智能, 机器人, Robot, Bot, 智能体, 提示词, Prompt, Agent, 智能体, GitHub, Work Flow, 论文, Paper', // 网站关键词 英文逗号隔开
